65cfd0e659e2c44a23f2a01d8df488b96d915860
reneilpascua <rpascua2@my.bcit.ca>
2020-06-20 18:41:13 -0700

    [q] fn to load the react app into page

---
+++ all-wp-files/react-build/precache-manifest.14c1d169c6684e0c371f1881c01112ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe8f33ad6a15729c49b1d3203fe8686",
+    "url": "/react-build/index.html"
+  },
+  {
+    "revision": "43f0654014f8affda612",
+    "url": "/react-build/static/css/main.655bc98f.chunk.css"
+  },
+  {
+    "revision": "ddc551e9611125a356bc",
+    "url": "/react-build/static/js/2.87c2ae52.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-build/static/js/2.87c2ae52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43f0654014f8affda612",
+    "url": "/react-build/static/js/main.53a3669f.chunk.js"
+  },
+  {
+    "revision": "b6e792c12b7c5999622c",
+    "url": "/react-build/static/js/runtime-main.066756ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-build/static/media/logo.5d5d9eef.svg"
+  }
+]);
