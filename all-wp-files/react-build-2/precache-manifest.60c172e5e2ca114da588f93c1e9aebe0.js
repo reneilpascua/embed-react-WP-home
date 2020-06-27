c30d58ab84b00bbca815747ac81ff838e4af60e1
reneilpascua <rpascua2@my.bcit.ca>
2020-06-26 19:44:50 -0700

    [q] tmsearch added on route /tmsearch

---
+++ all-wp-files/react-build-2/precache-manifest.60c172e5e2ca114da588f93c1e9aebe0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6929833b03c73d08dadc4192b9982a77",
+    "url": "/react-page-2/react-build-2/index.html"
+  },
+  {
+    "revision": "41b680598414883f6f93",
+    "url": "/react-page-2/react-build-2/static/css/main.655bc98f.chunk.css"
+  },
+  {
+    "revision": "ddc551e9611125a356bc",
+    "url": "/react-page-2/react-build-2/static/js/2.87c2ae52.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-page-2/react-build-2/static/js/2.87c2ae52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41b680598414883f6f93",
+    "url": "/react-page-2/react-build-2/static/js/main.cd5c72d6.chunk.js"
+  },
+  {
+    "revision": "703e8320362c56b045cb",
+    "url": "/react-page-2/react-build-2/static/js/runtime-main.bec850db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-page-2/react-build-2/static/media/logo.5d5d9eef.svg"
+  }
+]);
